1dd8906141db2bae4e74f1d74ce4e25eb174c763
Michael Kevin <michaelkmb@hotmail.com>
2023-09-20 11:54:49 -0300

    Project name change;

--- lab_project-pattern/src/edu/example/entity/Test.java
+++ lab_project-pattern/src/edu/example/entity/Test.java
@@ -3,15 +3,21 @@ package edu.example.entity;
 import edu.example.singleton.SingletonEager;
 import edu.example.singleton.SingletonLazy;
 import edu.example.singleton.SingletonLazyHolder;
+import edu.example.strategy.*;
 
 /*
 *
-* Testes relacionados ao modo Singleton de criação de instância
+* Testes relacionados aos padrões de projeto
 *
 * */
 public class Test {
 
     public static void main(String[] args) {
+
+        /*
+        *  Modo Singleton de criação de instância
+        * */
+
         SingletonLazy lazy = SingletonLazy.getInstance();
         SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstance();
         SingletonEager eager = SingletonEager.getInstance();
@@ -23,5 +29,25 @@ public class Test {
         eager = SingletonEager.getInstance();
         System.out.println("\nInstâncias 'alteradas'");
         System.out.println("Lazy: " + lazy + "\nLazyHolder: " + lazyHolder + "\nEager: " + eager);
+
+        /*
+         *  Modo estratégia de comportamento
+         * */
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
     }
 }
